e048eb123a0adadede056298976fff926cec7d40
예린 한 <hanyr@bimpeers.com>
2022-12-20 14:25:44 +0900

    #1 Mainpage 초기 마크업

--- portfolioclient/src/App.vue
+++ portfolioclient/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="MainPage_Div">
+  <div class="MainPage_Main">
+    <div class="MainPage_Tit">
+      <h3>YERIN<br>PORTFOLIO</h3>
+    </div>
+
+    <div class="MainPage_Overview">
+      <h4>Overview</h4>
+      <p>
+        안녕하세요. '낭만'을 항해하는 웹개발자 한예린입니다.<br>
+        이 포트폴리오에서는 퍼블리셔를 꿈꾸며 준비한,
+        저의 노력의 결과인 작업물들을 보실 수 있습니다.</p>
+    </div>
+  </div>
+  
+</div>
+
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import '../src/assets/css/mainpage.css'
 </style>
+
+
