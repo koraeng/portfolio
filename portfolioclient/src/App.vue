8b3b523a7503ced0c76534575e19ad21e256bf7f
예린 한 <hanyr@bimpeers.com>
2022-12-20 16:15:38 +0900

    #1 vue-router 설정 완료

--- portfolioclient/src/App.vue
+++ portfolioclient/src/App.vue
@@ -1,56 +1,56 @@
-<template>
-<div id="MainPage_Div">
-  <div class="MainPage_Main">
-    <div class="MenuBtn">
-      <p v-on:click="OpenMenu"></p>
-    </div>
-
-    <div class="MainPage_Menu_List">
-      <ul>
-        <li>
-          ABOUT
-          <p>Its' about my career, ability.<br>Click here to more</p>
-        </li>
-       
-        <li>
-          WEB Project
-          <p>Its' about my careerm ability.<br>Click here to discover</p>
-        </li>
-
-        <li>
-          Design Project
-          <p>Its' about signage, other design works.<br>Click here to discover</p>
-        </li>
-
-        <li>
-          Contact
-          <p>Wanna Contact with me?<br>Click here to contact</p>
-        </li>
-
-      </ul>
-    </div>
-    <div class="MainPage_Tit">
-      <h3>YERIN<br>PORTFOLIO</h3>
-    </div>
-
-    <div class="MainPage_Overview">
-      <h4>Overview</h4>
-      <p>
-        안녕하세요. '낭만'을 항해하는 웹개발자 한예린입니다.<br>
-        이 포트폴리오에서는 퍼블리셔를 꿈꾸며 준비한,
-        저의 노력의 결과인 작업물들을 보실 수 있습니다.</p>
-    </div>
-  </div>
-  
-</div>
-
+﻿<template>
+  <nav></nav>
+  <router-view />
 </template>
 
 <style>
-  @import '../src/assets/css/mainpage.css'
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
 
+<!--<script>
+    import axios from 'axios';
 
-<script>
+    export default {
+        data() {
+            return {
+                users: []
+            }
+        },
+        created() {
 
-</script>
+            axios.get('/api/weatherforecast', {
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    'Content-Type': 'application/json; charset = utf-8'
+                }
+            })
+                .then(response => {
+                    console.log(response.data);
+                })
+                .catch(e => {
+                    console.log('error : ', e)
+                })
+
+
+        }
+    }
+</script>-->
---
+++ portfolioclient/src/router/index.js
@@ -0,0 +1,23 @@
+import { createWebHistory, createRouter } from "vue-router";
+import Mainpage from "../views/MainPage.vue";
+import AboutPage from "../views/AboutPage.vue";
+
+const routes = [
+  {
+    path: "/main",
+    name: "Mainpage",
+    component: Mainpage,
+  },
+  {
+    path: "/about",
+    name: "AboutPage",
+    component: AboutPage,
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
