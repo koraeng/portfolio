3675c0c3c4909acc456fa783fc486e5d1bb5b3e3
예린 한 <hanyr@bimpeers.com>
2022-12-22 14:53:46 +0900

    #1  aboutpage 작업중

--- portfolioclient/src/views/AboutPage.vue
+++ portfolioclient/src/views/AboutPage.vue
@@ -1,8 +1,26 @@
 <template>
-  <div id="app">
-    <MainMenu />
-    <transition>
-      <router-view />
-    </transition>
+  <div id="AboutPage_Div">
+    <div class="AboutPage_Main">
+      <div class="AboutPage_Tit">
+        <img src="#" alt="" />
+        <ul class="MyInfo">
+          <li class="Name">HAN YERIN | Age 29</li>
+          <li>Web Developer & Web Publisher</li>
+          <div class="barS"></div>
+        </ul>
+
+        <ul class="MyAbility">
+          <li>Vue.js | Html | Css3 | javascript</li>
+          <li>Adobe Illustrator | Adobe PhotoShop | Figma</li>
+        </ul>
+        <div class="ResumeBtn">
+          <router-link to="#">Resume</router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
+
+<style>
+@import "../assets/css/about.css";
+</style>
